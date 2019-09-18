5045ccd6f6383f3fdea3b36517176631b3fcd283
Oleh Lutsyk <oleh2906@gmail.com>
2019-09-18 14:39:01 -0500

    cветлофор

--- lesson1/ViewController.swift
+++ lesson1/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstlight: UIView!
+    @IBOutlet weak var secondlight: UIView!
+    @IBOutlet weak var thirdlight: UIView!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        firstlight.layer.cornerRadius = 50
+        secondlight.layer.cornerRadius = 50
+        thirdlight.layer.cornerRadius = 50
+
+        button.layer.cornerRadius = 10
     }
 
+    @IBAction func button_pressed() {
+        
+        if firstlight.alpha < 0.4 && secondlight.alpha < 0.4   {
 
+
+        firstlight.alpha = 1
+            thirdlight.alpha = 0.3
+            button.setTitle("Next", for: .normal) }
+        else {
+            if secondlight.alpha < 0.4 && thirdlight.alpha < 0.4 {
+                
+                secondlight.alpha = 1
+                firstlight.alpha = 0.3}
+            else{
+                
+                if firstlight.alpha < 0.4 && thirdlight.alpha < 0.4 {
+                    
+                    thirdlight.alpha = 1
+                    secondlight.alpha = 0.3
+
+                }
+            }
+        
+        
+    }
+    
+}
 }
 
