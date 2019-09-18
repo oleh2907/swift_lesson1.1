9737aa4d9f86009a00d841c46761de381d2c7a50
Oleh Lutsyk <oleh2906@gmail.com>
2019-09-17 22:47:47 -0500

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Oleh Lutsyk on 9/17/19.
+//  Copyright © 2019 Oleh Lutsyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
